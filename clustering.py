057b4c2bca7e27f069f115fe8473ae0554a357b7
root <root@DESKTOP-DKL1HP3.localdomain>
2024-08-31 23:51:17 +0800

    fix error

--- clustering.py
+++ clustering.py
@@ -1,24 +1,25 @@
+# -*- coding: UTF-8 -*-
 import os, sys
 import numpy as np
 import pandas as pd
 import csv
 
 if len(sys.argv) < 4:
-    print("Usage: python match.py {cadidate_path} {FPS_POS_path} {FPS_NEG_path} ")
+    print("Usage: python clustering.py {candidate_path} {FPS_POS_path} {FPS_NEG_path} ")
     sys.exit(-1)
 
 """
 Input data in CSV file
 """
-cadidate_file = sys.argv[1]
+candidate_file = sys.argv[1]
 pos_result_file = sys.argv[2]
 neg_result_file = sys.argv[3]
 
 # 1. read all file content
-## 1.1 read cadidate
-cadidate = pd.read_csv(cadidate_file)
-# print(cadidate)
-# cadidate may look like:
+## 1.1 read candidate
+candidate = pd.read_csv(candidate_file)
+# print(candidate)
+# candidate may look like:
 #       ID         Rt    Mz_POS  Size
 # 0    330   3.653100  417.1365    61
 # 1    345   3.745017  465.1032   104
@@ -30,21 +31,21 @@ fps_pos = pd.read_csv(pos_result_file)
 ## 1.3 read FPS_NEG
 fps_neg = pd.read_csv(neg_result_file)
 
-# 2. now we get the MZ_POS value of cadidates
-candidate_mz_pos = cadidate["Mz_POS"]
+# 2. now we get the MZ_POS value of candidates
+candidate_mz_pos = candidate["Mz_POS"]
 # print(candidate_mz_pos)
 
 may_same_collector_pos = []
-# 3. Then we match cadidates from FPS_POS with mz_pos value
+# 3. Then we match candidates from FPS_POS with mz_pos value
 for candidate_idx, mz_pos in candidate_mz_pos.iteritems():
     # calc mz_pos resonable value range
     tolerance = 0.05   # may passed by argument
     lower_bound = mz_pos - tolerance
     upper_bound = mz_pos + tolerance
-    # print("lower_bound:%f, upper_bound: %f" % (lower_bound, upper_bound))
+    #print("lower_bound: %f, upper_bound: %f" % (lower_bound, upper_bound))
     may_same = []
     # get FPS_POS mz_pos data
-    fps_pos_values = fps_pos[["Precursor m/z", "RT (min)", "Height"]]
+    fps_pos_values = fps_pos[["Precursor m/z", "RT (min)", "Area"]]
     for idx, pos_row in fps_pos_values.iterrows():
         mz_value = pos_row["Precursor m/z"]
         if mz_value >= lower_bound and mz_value <= upper_bound:
@@ -70,7 +71,7 @@ for candidate_idx, mz_neg in candidate_mz_neg.iteritems():
     # print("lower_bound:%f, upper_bound: %f" % (lower_bound, upper_bound))
     may_same = []
     # get FPS_NEG mz_neg data
-    fps_neg_values = fps_neg[["Precursor m/z", "RT (min)", "Height"]]
+    fps_neg_values = fps_neg[["Precursor m/z", "RT (min)", "Area"]]
     for idx, neg_row in fps_neg_values.iterrows():
         mz_value = neg_row["Precursor m/z"]
         if mz_value >= lower_bound and mz_value <= upper_bound:
@@ -111,30 +112,30 @@ for i in range(0, len(may_same_collector_pos)):
 # print(matched_pos_and_neg)
 # print("###############################")
 
-# 6. calc pos_height/neg_height
-pos_height_divide_neg_height = []
+# 6. calc pos_area/neg_area
+pos_area_divide_neg_area = []
 for pos_and_neg_list in matched_pos_and_neg:
     result_list = []
     for pos_and_neg in pos_and_neg_list:
-        ratio = pos_and_neg[0]["Height"] / pos_and_neg[1]["Height"]
+        ratio = pos_and_neg[0]["Area"] / pos_and_neg[1]["Area"]
         if ratio < 1:
             ratio = -1 / ratio
         result_list.append(ratio)
-    pos_height_divide_neg_height.append(result_list)
-# print(pos_height_divide_neg_height)
+    pos_area_divide_neg_area.append(result_list)
+# print(pos_area_divide_neg_area)
 
 # 7. cluster into 'Anthocyanin' and 'Flavonol glycoside'
 ## 'Anthocyanin': > 4.5
 ## 'Flavonol glycoside': < -1
 candidate_idx = 0
-header = ['candidate_index', 'Ratio', 'Cluster', 'pos_id', 'pos_mz', 'pos_rt',
-          'pos_height', 'neg_id', 'neg_mz', 'neg_rt', 'neg_height']
+header = ['Candidate_index', 'POS_RT', 'POS_Precursor',
+          'POS_Area', 'NEG_RT', 'NEG_Precursor', 'NEG_Area', 'Ratio', 'Cluster']
 data = []
-for value_list in pos_height_divide_neg_height:
+for value_list in pos_area_divide_neg_area:
     # print("Matched Results for Candidate#%d" % candidate_idx)
     pos_and_neg_list = matched_pos_and_neg[candidate_idx]
     if len(value_list) == 0:
-        print("No Matched Cands found!")
+        print("No Matched Candidates found!")
     value_idx = 0
     for value in value_list:
         pos_item = pos_and_neg_list[value_idx][0]
@@ -142,19 +143,15 @@ for value_list in pos_height_divide_neg_height:
         value_idx += 1
         cluster = 'Unknown'
         if value > 4.5:
-            cluster = 'Anthocyanin'
+            cluster = 'A'
         elif value < -1:
-            cluster = 'Flavonol glycoside'
-        data.append([candidate_idx, value, cluster, pos_item.name, pos_item["Precursor m/z"], pos_item["RT (min)"],
-                     pos_item["Height"], neg_item.name, neg_item["Precursor m/z"], neg_item["RT (min)"], neg_item["Height"]])
-        # print("Ratio:%f, cluster:%s, pos_info:{\"id\":%s, \"mz\":%f, \"rt\":%f, \"height\":%f},"
-        #     "neg_info:{\"id\":%s, \"mz\":%f, \"rt\":%f, \"height\":%f}" % 
-        #     (value, cluster, pos_item.name, pos_item["Precursor m/z"], pos_item["RT (min)"],
-        #     pos_item["Height"], neg_item.name, neg_item["Precursor m/z"], neg_item["RT (min)"],
-        #     neg_item["Height"]))
+            cluster = 'F'
+        data.append([candidate_idx, pos_item["RT (min)"], pos_item["Precursor m/z"],
+                     pos_item["Area"], neg_item["RT (min)"], neg_item["Precursor m/z"], neg_item["Area"],
+                     value, cluster])
     candidate_idx += 1
-    print("")
-    
+    # print("")
+
 # 8. Write data to CSV file
 with open('result.csv', 'w', newline='') as f:
     writer = csv.writer(f)
